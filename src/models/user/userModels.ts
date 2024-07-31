423c7c6a249149e195da3cf6f807a6ac355f5218
FreelancerMamun <mamunkhan16345@gmail.com>
2024-08-01 01:38:51 +0600

    last update

---
+++ src/models/user/userModels.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { User, User } from "./userTypes";
+const userSchemna = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<User>("User", userSchemna);
+
+export default User;
