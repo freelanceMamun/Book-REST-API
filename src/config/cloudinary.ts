f9d2b13a9af9d3145f6bd059647d542980839f0f
FreelancerMamun <mamunkhan16345@gmail.com>
2024-08-03 15:57:18 +0600

    file upload

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary"; // Configuration
+
+cloudinary.config({
+  cloud_name: "daav4j7jt",
+  api_key: "282549337329877",
+  api_secret: "9c4MFL241BshaJd8J8gBSfm-N4I", // Click 'View Credentials' below to copy your API secret
+});
+
+export default cloudinary;
